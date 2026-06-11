447dbe982881594d4b1a003893fe75169bac9230
Sushruttiwari <sushruttiwari309@gmail.com>
2026-06-11 16:53:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from pathlib import Path
 app = FastAPI()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-model = joblib.load(BASE_DIR / "models" / "sales_model.pkl")
+model = joblib.load(BASE_DIR / "sales_model.pkl")
 
 class SalesInput(BaseModel):
     Store: int
